f36721a387cdc8d5b3fcabb3a51af2443dfba1ab
Olesia Pashko <pashkoolesia1234@gmail.com>
2020-11-13 12:48:21 +0200

    add screenshots

--- Project-frontend/src/app/auth/auth.guard.ts
+++ Project-frontend/src/app/auth/auth.guard.ts
@@ -13,12 +13,12 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot):  boolean {
-  		if(localStorage.getItem('token')!=null)
+  		//if(localStorage.getItem('token')!=null)
     		return true;
-    	else{
-    		this.router.navigate(['user/login']);
-    		return false;
-    	}
+    	//else{
+    	//	this.router.navigate(['user/login']);
+    	//	return false;
+    	//}
   }
   
 }
